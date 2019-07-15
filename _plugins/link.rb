fe7a15f35a5b0f0ebaf19ffcf58b5e41448760e4
deepu105 <d4udts@gmail.com>
2019-07-15 22:42:35 +0200

    add link stub

---
+++ _plugins/link.rb
@@ -0,0 +1,12 @@
+# A simple stub for the Dev.to link tag
+class LinkTag < Liquid::Tag
+    def initialize(_tag_name, url, _tokens)
+      @article = url
+    end
+  
+    def render(_context)
+    "<a href='#{@article}'>#{@article}</a>"
+    end
+  end
+  
+  Liquid::Template.register_tag("link", LinkTag)
