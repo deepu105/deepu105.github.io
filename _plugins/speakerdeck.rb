ef5ab1275dbb70b4841694850c77ada3e19919f8
deepu105 <d4udts@gmail.com>
2019-07-16 18:16:27 +0200

    fix speakerdeck tag plugin

--- _plugins/speakerdeck.rb
+++ _plugins/speakerdeck.rb
@@ -2,24 +2,24 @@ module Jekyll
     class Speakerdeck < Liquid::Tag
       @@width = 640
       @@height = 510
-  
+
       def initialize(name, id, tokens)
         super
         @id = id
       end
-  
+
       def render(context)
         @id_stripped = @id.strip.tr('"', '')
         %(<p>
-            <script async="async" class="speakerdeck-embed" data-id=#{@id} data-ratio="1.77777777777778" src="//speakerdeck.com/assets/embed.js"> </script>
+            <script async class="speakerdeck-embed" data-id="#{@id_stripped}" data-ratio="1.77777777777778" src="//speakerdeck.com/assets/embed.js"></script>
             <noscript>
                 <a href="//speakerdeck.com/player/#{@id_stripped}">
-                <img alt="Folien bei Speakerdeck" src="//speakerd.s3.amazonaws.com/presentations/#{@id_stripped}/slide_0.jpg">
+                <img alt="Speakerdeck" src="//speakerd.s3.amazonaws.com/presentations/#{@id_stripped}/slide_0.jpg">
                 </a>
             </noscript>
           </p>)
       end
     end
   end
-  
+
   Liquid::Template.register_tag('speakerdeck', Jekyll::Speakerdeck)
