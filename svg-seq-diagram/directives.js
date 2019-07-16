c57860333f9c93b58a1a7a432d395b1941f149d9
deepu105 <d4udts@gmail.com>
2019-07-16 14:04:21 +0200

    add svg generator back

---
+++ svg-seq-diagram/directives.js
@@ -0,0 +1,10 @@
+angular.module('myApp')
+    .directive('vbox', function() {
+        return {
+            link: function(scope, element, attrs) {
+                attrs.$observe('vbox', function(value) {
+                    element.attr('viewBox', value);
+                })
+            }
+        };
+    });
