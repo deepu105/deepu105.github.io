eac02aab999dfb3adc97b401711980ac007e1fde
deepu105 <d4udts@gmail.com>
2019-07-16 15:14:03 +0200

    add publish script

--- publish.sh
+++ publish.sh
@@ -3,13 +3,29 @@
 rm -rf _site
 
 if [ -z "$(git status --porcelain)" ]; then
-  # Working directory clean
+    # Working directory clean
+    TMP_LOC=/tmp/deepu.github.io
+    # Build site
     bundle update listen
     bundle exec jekyll build
 
-    git checkout master
+    # Move to temp
+    mkdir --parents $TMP_LOC
+    mv _site/* $TMP_LOC
 
+    #  CLean directory
+    git checkout master
     rm -rf *
+
+    # Move site form temp & publish
+    mv $TMP_LOC/* .
+    git add --all
+    git commit -am "Updated site on $now"
+    git push origin master
+
+    echo "$now: Published changes to GitHub"
+
+    git checkout site_src
 else
     echo "Working directory is not clean. Commit changes!"
     exit
