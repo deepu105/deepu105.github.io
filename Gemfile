4e06234c30b61569b0e859d0136f92437eca49c4
deepu105 <d4udts@gmail.com>
2019-07-18 09:33:36 +0200

    add read time

--- Gemfile
+++ Gemfile
@@ -10,13 +10,6 @@ source "https://rubygems.org"
 # Happy Jekylling!
 gem "jekyll", "~> 3.8.6"
 
-# Hello! This is where you manage which Jekyll version is used to run.
-# When you want to use a different version, change it below, save the
-# file and run `bundle install`. Run Jekyll with `bundle exec`, like so:
-#
-#     bundle exec jekyll serve
-#
-
 # If you have any plugins, put them here!
 gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 group :jekyll_plugins do
---
+++ _plugins/reading_time_filter.rb
@@ -0,0 +1,20 @@
+# Read this in "about 4 minutes"
+# Usage: Read this in {{ page.content | reading_time }}
+
+WORD_PER_MINUTE = 180
+
+def calculate_time( input )
+  words = input.split.size;
+  minutes = ( words / WORD_PER_MINUTE ).floor
+  minutes_label = minutes == 1 ? "min" : "mins"
+  return minutes, minutes_label
+end
+
+module ReadingTimeFilter
+  def reading_time( input )
+    minutes, minutes_label = calculate_time(input)
+    minutes > 0 ? "#{minutes} #{minutes_label} read" : "Less than 1 min read"
+  end
+
+  Liquid::Template.register_filter(ReadingTimeFilter)
+end
